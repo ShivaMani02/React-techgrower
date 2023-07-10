1704bbedcd8978d084bc23f6183db8c79f7a4dd5
SHIVANSH VASU <86513644+ShivaMani02@users.noreply.github.com>
2023-07-10 15:28:03 +0530

    Add files via upload
    Project

---
+++ src/components/Home.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>TechGrower</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+          Welcome to our technical website, where we strive to provide you with the latest and most valuable information in the world of technology. Our team of experts is passionate about all things tech, and we are dedicated to bringing you accurate, reliable, and up-to-date content.
+
+At our website, we cover a wide range of topics, catering to both beginners and advanced tech enthusiasts. Whether you're looking for beginner's guides, in-depth technical analyses, product reviews, or troubleshooting tips, you'll find it all here. We believe that technology should be accessible to everyone, and our content reflects that commitment.
+
+Our team consists of experienced professionals who have a deep understanding of various technological domains. From software development and artificial intelligence to hardware components and networking, we have experts in all areas to ensure that we deliver comprehensive and well-rounded information.
+
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
